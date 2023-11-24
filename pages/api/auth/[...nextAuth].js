97610b24b549025747ce5edc96ee237d2a282f77
bemanima <bemanima@gmail.com>
2023-11-24 14:09:06 +0330

    Add next auth Add next auth configuration

---
+++ pages/api/auth/[...nextAuth].js
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers";
+
+import User from "@/models/User";
+import { verifyPassword } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  secret: process.env.SECRET_KEY,
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("Error in connecting to DB!");
+        }
+
+        if (!email || !password) throw new Error("Invalid data");
+
+        const user = await User.findOne({ email: email });
+
+        if (!user) throw new Error("User doesn't exist!");
+
+        const isValid = verifyPassword(password, user.password);
+
+        if (!isValid) throw new Error("Username or password is incorrect!");
+
+        return { email };
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
