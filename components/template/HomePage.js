f8bf88ced5a2655fce754235c81b6ba2d9da5037
bemanima <bemanima@gmail.com>
2023-11-24 17:03:58 +0330

    Add tasks card

--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Tasks from "../module/Tasks";
 
 const HomePage = () => {
   const [todos, setTodos] = useState();
@@ -12,20 +13,25 @@ const HomePage = () => {
     const data = await res.json();
 
     if (data.status === "success") setTodos(data.data.todos);
+    console.log(todos);
   };
   return (
     <div className="home-page">
       <div className="home-page--todo">
         <p>Todo</p>
+        <Tasks data={todos?.todo} />
       </div>
       <div className="home-page--inProgress">
         <p>In Progress</p>
+        <Tasks data={todos?.inProgress} />
       </div>
       <div className="home-page--review">
         <p>Review</p>
+        <Tasks data={todos?.review} />
       </div>
       <div className="home-page--done">
         <p>Done</p>
+        <Tasks data={todos?.done} />
       </div>
     </div>
   );
