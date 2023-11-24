5a96cfd2644f661f9931a890c7667cc16bea7bff
bemanima <bemanima@gmail.com>
2023-11-24 14:37:48 +0330

    Add create todos Api

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
+import { getSession } from "next-auth/react";
 import styles from "./index.module.css";
 const index = () => {
   return <div>Home page</div>;
 };
 
 export default index;
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
